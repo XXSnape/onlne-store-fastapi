5b53cb529927841f0de9f9351732743665cca574
nachi <serg.serg2006@mail.ru>
2025-02-01 20:32:52 +0300

    init commit

---
+++ django/urls.py
@@ -0,0 +1,45 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="frontend2/index.html")),
+    path("about/", TemplateView.as_view(template_name="frontend2/about.html")),
+    path("account/", TemplateView.as_view(template_name="frontend2/account.html")),
+    path("cart/", TemplateView.as_view(template_name="frontend2/cart.html")),
+    path("catalog/", TemplateView.as_view(template_name="frontend2/catalog.html")),
+    path(
+        "catalog/<int:id>/",
+        TemplateView.as_view(template_name="frontend2/catalog.html"),
+    ),
+    path(
+        "history-order/",
+        TemplateView.as_view(template_name="frontend2/historyorder.html"),
+    ),
+    path(
+        "order-detail/<int:id>/",
+        TemplateView.as_view(template_name="frontend2/oneorder.html"),
+    ),
+    path(
+        "orders/<int:id>/", TemplateView.as_view(template_name="frontend2/order.html")
+    ),
+    path(
+        "payment/<int:id>/",
+        TemplateView.as_view(template_name="frontend2/payment.html"),
+    ),
+    path(
+        "payment-someone/",
+        TemplateView.as_view(template_name="frontend2/paymentsomeone.html"),
+    ),
+    path(
+        "product/<int:id>/",
+        TemplateView.as_view(template_name="frontend2/product.html"),
+    ),
+    path("profile/", TemplateView.as_view(template_name="frontend2/profile.html")),
+    path(
+        "progress-payment/",
+        TemplateView.as_view(template_name="frontend2/progressPayment.html"),
+    ),
+    path("sale/", TemplateView.as_view(template_name="frontend2/sale.html")),
+    path("sign-in/", TemplateView.as_view(template_name="frontend2/signIn.html")),
+    path("sign-up/", TemplateView.as_view(template_name="frontend2/signUp.html")),
+]
---
+++ front.py
@@ -0,0 +1,65 @@
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from fastapi import APIRouter
+from starlette.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(
+    directory="frontend/templates",
+)
+
+
+class A:
+    is_authenticated = True
+
+
+@router.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "user": A()})
+
+
+@router.get("about/")
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request, "user": A()})
+
+
+@router.get("account/")
+async def account(request: Request):
+    return templates.TemplateResponse("account.html", {"request": request, "user": A()})
+
+
+@router.get("cart/")
+async def cart(request: Request):
+    return templates.TemplateResponse("cart.html", {"request": request, "user": A()})
+
+
+@router.get("catalog/")
+async def catalog(request: Request):
+    return templates.TemplateResponse("catalog.html", {"request": request, "user": A()})
+
+
+@router.get("catalog/{id}/")
+async def about(request: Request):
+    return templates.TemplateResponse("catalog.html", {"request": request, "user": A()})
+
+
+@router.get("history-order/")
+async def history(request: Request):
+    return templates.TemplateResponse(
+        "historyorder.html", {"request": request, "user": A()}
+    )
+
+
+@router.get("order-detail/{id}/")
+async def about(request: Request):
+    return templates.TemplateResponse("catalog.html", {"request": request, "user": A()})
+
+
+@router.get("about/")
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request, "user": A()})
+
+
+@router.get("about/")
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request, "user": A()})
