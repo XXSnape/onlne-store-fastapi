a99719bb079e2b3c6a4928b95b84ee6d4ab68edd
Snape <snape@example.com>
2025-06-06 22:27:37 +0300

    write tests to get categories

---
+++ tests/catalogs/test_categories.py
@@ -0,0 +1,19 @@
+from httpx import AsyncClient
+
+
+async def test_get_categories(ac: AsyncClient):
+    response = await ac.get("api/categories")
+    assert response.json() == [
+        {
+            "id": 1,
+            "title": "Category1",
+            "image": None,
+            "subcategories": [
+                {"id": 6, "title": "Category5", "image": None},
+                {"id": 5, "title": "Category4", "image": None},
+                {"id": 4, "title": "Category3", "image": None},
+                {"id": 3, "title": "Category2", "image": None},
+            ],
+        },
+        {"id": 2, "title": "Category6", "image": None, "subcategories": []},
+    ]
