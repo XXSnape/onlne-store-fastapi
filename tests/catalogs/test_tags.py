bd5d2085a2b445c83d8a52b8aba24f0b27775a4c
Snape <snape@example.com>
2025-06-06 21:41:47 +0300

    write more tests for tags

--- tests/catalogs/test_tags.py
+++ tests/catalogs/test_tags.py
@@ -1,11 +1,35 @@
+import pytest
 from httpx import AsyncClient
 
 
-async def test_get_tags(ac: AsyncClient):
-    response = await ac.get("api/tags")
-    print("DATA", response.json())
-    assert response.json() == [
-        {"id": 1, "name": "Tag1"},
-        {"id": 2, "name": "Tag2"},
-        {"id": 3, "name": "Tag3"},
-    ]
+@pytest.mark.parametrize(
+    "params, result",
+    [
+        (
+            None,
+            [
+                {"id": 1, "name": "Tag1"},
+                {"id": 2, "name": "Tag2"},
+                {"id": 3, "name": "Tag3"},
+            ],
+        ),
+        (
+            {"category": 1},
+            [
+                {"id": 1, "name": "Tag1"},
+                {"id": 2, "name": "Tag2"},
+            ],
+        ),
+        (
+            {"category": 100},
+            [],
+        ),
+    ],
+)
+async def test_get_tags(
+    ac: AsyncClient,
+    params: dict[str, str] | None,
+    result: list[dict[str, str | int]],
+):
+    response = await ac.get("api/tags", params=params)
+    assert response.json() == result
