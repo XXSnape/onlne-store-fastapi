536147ccf35366753bed926f592c32f3a8fada86
Snape <snape@example.com>
2025-06-10 19:43:12 +0300

    add upload_data function

--- tests/shop/data.py
+++ tests/shop/data.py
@@ -1,3 +1,13 @@
+from pathlib import Path
+from json import load
+
+
+def upload_data(filename: str) -> dict | list:
+    path = Path(__file__).resolve().parent / "data" / filename
+    with path.open() as file:
+        return load(file)
+
+
 def clean_dates(objects: list[dict], attrs=("date",)):
     for obj in objects:
         for attr in attrs:
